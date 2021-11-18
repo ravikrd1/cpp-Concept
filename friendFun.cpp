d7e74492fe847b102312df235903ad2d104f95d9
Ravi <student.ravikr@gmail.com>
2021-11-18 19:46:12 +0530

    Friend function usage

---
+++ friendFun.cpp
@@ -0,0 +1,45 @@
+//friend class usage
+
+#include <iostream>
+using namespace std;
+
+class Complex{
+      int a,b;
+    public:
+  
+     void setdata(int x,int y)
+     {
+         a=x;
+         b=y;
+     }
+     
+     void showdata()
+     {
+         cout<<"a="<<a<<"  b="<<b<<endl;
+     }
+     
+     friend ostream& operator<<(ostream&, Complex);
+     friend istream& operator>>(istream&, Complex&);
+};
+
+ostream& operator <<(ostream& dout,Complex C)
+{
+    cout<<"a="<<C.a<<" b="<<C.b<<endl;
+    
+    return(dout);
+}
+istream& operator >> (istream & din,Complex& C )
+{
+    cin>>C.a>>C.b;
+    return(din);
+}
+int main()
+{
+    cout<<"Enter Complex Number:";
+    Complex c1;
+    cin>>c1;
+    cout<<"Complex Number is:"<<endl;
+    cout<<c1;
+
+    return 0;
+}
